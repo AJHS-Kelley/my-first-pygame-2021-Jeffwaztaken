a970e03a9763a329318d8610bae0f180c5871c68
Jeffwaztaken <90346679+Jeffwaztaken@users.noreply.github.com>
2022-01-05 14:49:31 -0500

    0.1 4A
    importing module for pygame

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with PyGame, Zeigler Jeffrey, 1/5/22, 2:28PM, v0.0
+# Simple Animation with PyGame, Zeigler Jeffrey, 1/5/22, 2:48PM, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup Pygame
+pygame.init()
+
+# Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowsurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!') 
